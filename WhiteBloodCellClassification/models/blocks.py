11cbc8984dd4b3f2dddcda9ddc2ab5440d6a7f3c
Xuan Hoang <tranxuanhoang14062004@gmail.com>
2026-03-13 20:51:35 +0700

    add Col-layer

--- WhiteBloodCellClassification/models/blocks.py
+++ WhiteBloodCellClassification/models/blocks.py
@@ -11,3 +11,14 @@ class ConvBlock(nn.Module):
         )
     def forward(self, x):
         return self.block(x)
+
+class MLPLayer(nn.Module):
+    def __init__(self,in_channels, hidden_dim,out_channels):
+        super().__init__()
+        self.mlp = nn.Sequential(
+            nn.Conv2d(in_channels, hidden_dim, kernel_size=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(hidden_dim, out_channels, kernel_size=1)
+        )
+    def forward(self, x):
+        return self.mlp(x)
